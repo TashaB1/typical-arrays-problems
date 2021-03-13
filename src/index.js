23749a0d4f55fc27fec85ba2228ce6f6e9c7ac57
TashaB1 <tasha2k7@mail.ru>
2021-03-13 12:14:35 +0300

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+     let min = 0;
+
+     if ((Array.isArray(array))&&(array.length > 0)){
+        min = array[0];
+
+        for (i=0; i<array.length; i++){
+            if (array[i]<min) {
+                min = array[i];
+            }
+        }
+     }
+
+     return min;
 }
 
+
 exports.max = function max (array) {
-  return 0;
+      let max = 0;
+
+      if ((Array.isArray(array)) && (array.length > 0)){
+         max = array[0];
+
+         for (i=0; i<array.length; i++){
+             if (array[i]>max) {
+                 max = array[i];
+             }
+         }
+      }
+
+      return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((Array.isArray(array))&&(array.length > 0)){
+        return array.reduce((a, b) => (a + b)) / array.length;
+    }
+    else{
+        return 0;
+    }
 }
